65dbe821d5ce533d3cb4f63276172b7fd3c432de
jhnr <766012090@qq.com>
2024-07-08 17:50:32 +0800

    feat: 新增更改分类功能

--- src/renderer/src/pages/Content/ContentLoader.ts
+++ src/renderer/src/pages/Content/ContentLoader.ts
@@ -1,4 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-export default ({ params }) => {
-  return window.api.sql(`SELECT * FROM contents WHERE id = ${params.id}`, 'findOne')
+export default async ({ params }) => {
+  const content = await window.api.sql(`SELECT * FROM contents WHERE id = ${params.id}`, 'findOne')
+  const categories = await window.api.sql(`SELECT * FROM categories`, 'findAll')
+  return {
+    content,
+    categories,
+  }
 }
--- src/renderer/src/pages/ContentList/ContentListAction.ts
+++ src/renderer/src/pages/ContentList/ContentListAction.ts
@@ -1,14 +1,16 @@
+import { redirect } from 'react-router-dom'
+
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export default async ({ request, params }) => {
   const formData = await request.formData()
   const cid = params.cid || 0
-  console.log(cid)
   switch (formData.get('action')) {
     case 'add': {
-      window.api.sql(
+      const id = await window.api.sql(
         `INSERT INTO contents (title, content, category_id, created_at) VALUES ('未命名片段', '', ${cid}, datetime());`,
         'insert',
       )
+      return redirect(`content/${id}`)
     }
   }
   return {}
